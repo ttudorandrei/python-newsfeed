67d72a7e8e4a8c50bb28401b8df01cf3616f3390
Tudor Tocan <tudor.tocan@gmail.com>
2021-10-03 17:42:43 +0100

    register dashboard route on the server

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from .routes import home
+from .routes import home, dashboard
 
 
 def create_app(test_config=None):
@@ -17,5 +17,6 @@ def create_app(test_config=None):
 
     # register routes
     app.register_blueprint(home)
+    app.register_blueprint(dashboard)
 
     return app
