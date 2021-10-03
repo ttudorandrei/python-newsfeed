77394d960a540a735488ba328701df955bafdb53
Tudor Tocan <tudor.tocan@gmail.com>
2021-10-03 18:09:04 +0100

    create User model

---
+++ app/models/User.py
@@ -0,0 +1,12 @@
+import sqlalchemy
+from sqlalchemy.sql.expression import null
+from app.db import Base
+from sqlalchemy import Column, Integer, String
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = column(String(50), nullable=False)
+    email = Column(String(50), nullable=False, unique=True)
+    password = Column(String(100), nullbale=False)
