4257ad52f3547f490ff7afd8c112596c3dbdc336
Tudor Tocan <tudor.tocan@gmail.com>
2021-10-03 18:41:37 +0100

    add email, paswword validation and password hashing

--- app/models/User.py
+++ app/models/User.py
@@ -1,7 +1,12 @@
 import sqlalchemy
 from sqlalchemy.sql.expression import null
-from app.db import Base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import validates
+import bcrypt
+from app.db import Base
+
+
+salt = bcrypt.gensalt()
 
 
 class User(Base):
@@ -10,3 +15,16 @@ class User(Base):
     username = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False, unique=True)
     password = Column(String(100), nullable=False)
+
+    @validates("email")
+    def validate_email(self, key, email):
+        # only accept if email address contains an "@" sign
+        assert "@" in email
+
+        return email
+
+    @validates("password")
+    def validate_password(self, key, password):
+        assert len(password) > 4
+
+        return bcrypt.hashpw(password.encode("utf-8"), salt)
