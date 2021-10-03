660d2bdb50f06514cc653c7370d8b07ce44f09b3
Tudor Tocan <tudor.tocan@gmail.com>
2021-10-03 20:43:43 +0100

    add code to get all posts on homepage, close db connection after querying for whatever it needs

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -15,12 +15,24 @@ Session = sessionmaker(bind=engine)
 Base = declarative_base()
 
 
-def init_db():
+def init_db(app):
     Base.metadata.create_all(engine)
+
+    app.teardown_appcontext(close_db)
 
 
 def get_db():
     if "db" not in g:
+
         # store db connection in app context
         g.db = Session()
         return g.db
+
+
+def close_db(e=None):
+    # attempts to find and remove db from the g object
+    db = g.pop("db", None)
+
+    # if dbd exists (is not none) close connection
+    if db is not None:
+        db.close()
